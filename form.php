264fa5fdde24146cf57c30efc3d52027c88478cd
BanterGraphics <bantergraphics@gmail.com>
2024-11-20 15:48:02 +0300

    Upload

---
+++ form.php
@@ -0,0 +1,15 @@
+<?php
+if (isset($_POST['email'])) {
+
+    // REPLACE THIS 2 LINES AS YOU DESIRE
+    $email_to = "info@netrichorsystems.net";
+    $email_subject = "You've got a new submission";
+?>
+
+    <!-- Replace this as your success message -->
+
+    Thanks for contacting us, we will get back to you as soon as possible.
+
+<?php
+}
+?>
---
+++ subscriberform.php
@@ -0,0 +1,5 @@
+<?php
+if($_POST["Message"]) {
+mail("info@netrichorsystems.net", "Unsubscribe")
+}
+?>
